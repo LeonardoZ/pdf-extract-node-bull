9b61b587d9c9cd5529653c5d84a6ba6b63b7e2f3
Leonardo Zapparoli <leo.zapparoli@gmail.com>
2021-09-25 16:07:39 -0300

    Finishs basic setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
-const knex = require("knex")({
-  client: "database",
+const knex = require('knex')({
+  client: 'mysql',
   connection: {
-    host: "127.0.0.1",
+    host: '127.0.0.1',
     port: 3306,
-    user: "your_database_user",
-    password: "your_database_password",
-    database: "myapp_test",
+    user: 'root',
+    password: '1234',
+    database: 'pdf-extraction-db',
   },
 });
+
+module.exports = knex;
